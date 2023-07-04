ba2b1f382a705fd300a6c4eea879e96956d2eb0d
Francis1a <52089780+Francis1a@users.noreply.github.com>
2023-07-04 20:18:08 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import { DarkModeContextProvider } from "./context/darkModeContext";
 import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.render(
-  <DarkModeContextProvider>
-    <AuthContextProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </AuthContextProvider>
-  </DarkModeContextProvider>,
+    <React.StrictMode>
+      <DarkModeContextProvider>
+        <AuthContextProvider>
+            <App />
+        </AuthContextProvider>
+      </DarkModeContextProvider>
+    </React.StrictMode>,
   document.getElementById("root")
 );
