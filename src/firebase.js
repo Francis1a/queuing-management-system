19977c46c1f1faed94a32cb664650372c7296b20
Francis1a <52089780+Francis1a@users.noreply.github.com>
2023-07-23 18:59:16 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 
@@ -15,6 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+
 export const db = getFirestore();
 
 
